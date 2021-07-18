9b90b190c3c98408dd33dd85f57b0ff335bd7969
Steven van den Broek <30909373+Yvee1@users.noreply.github.com>
2021-07-18 22:41:08 +0200

    Update hascard to v0.5.0.3

--- hascard.rb
+++ hascard.rb
@@ -1,8 +1,8 @@
 class Hascard < Formula
   desc "A TUI for reviewing notes using 'flashcards' written with markdown-like syntax"
   homepage "https://github.com/Yvee1/hascard"
-  url "https://github.com/Yvee1/hascard/releases/download/v0.5.0.2/hascard-v0.5.0.2-osx.tar.gz"
-  sha256 "546980c513bdcb5c13d2de461e01d2fe18dd2987990206350f1ef2fcd2bc36dd"
+  url "https://github.com/Yvee1/hascard/releases/download/v0.5.0.3/hascard-v0.5.0.3-osx.tar.gz"
+  sha256 "fc6c9c3002dc8cd6f2079c66fcacc645dd0ac5dbacc9c608fe7fbb914a0e5ea2"
 
   bottle :unneeded
 
