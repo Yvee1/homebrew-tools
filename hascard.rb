c09d8bf3fe9fa5e73dab3270e615338019a1f4dd
Steven van den Broek <30909373+Yvee1@users.noreply.github.com>
2024-10-27 20:57:24 +0100

    Update hascard to v0.6.0.2

--- hascard.rb
+++ hascard.rb
@@ -1,8 +1,8 @@
 class Hascard < Formula
   desc "A TUI for reviewing notes using 'flashcards' written with markdown-like syntax"
   homepage "https://github.com/Yvee1/hascard"
-  url "https://github.com/Yvee1/hascard/releases/download/v0.6.0.1/hascard-v0.6.0.1-osx.tar.gz"
-  sha256 "4641a8f70c2721d6675f864369a284ce2e1028464067a6d95f8d3e0ec5c33873"
+  url "https://github.com/Yvee1/hascard/releases/download/v0.6.0.2/hascard-v0.6.0.2-osx.tar.gz"
+  sha256 "9b585d05a7357b5e8cd817ebb875e5280cb398672804605916fee058dbdb6afc"
 
   def install
     bin.install "hascard"
